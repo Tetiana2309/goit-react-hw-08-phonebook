750384c5c8ab5676b46b277c19e55ef6cd58c0ef
Tetiana2309 <126595700+Tetiana2309@users.noreply.github.com>
2023-11-01 04:57:17 +0100

    ok
    ok

--- src/components/ContactDataUpdateForm/ContactDataUpdateForm.jsx
+++ src/components/ContactDataUpdateForm/ContactDataUpdateForm.jsx
@@ -62,7 +62,7 @@ export const ContactDataUpdateForm = ({ onCloseModal, updateContactId }) => {
             type="text"
             name="name"
             value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]{4,8}*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+([' -]?[a-zA-Zа-яА-Я ]?[a-zA-Zа-яА-Я]{4,8})*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -74,7 +74,7 @@ export const ContactDataUpdateForm = ({ onCloseModal, updateContactId }) => {
             value={number}
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
+            pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             placeholder="Введіть номер"
--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -46,7 +46,7 @@ export const Phonebook = ({ onAddContact }) => {
             type="text"
             name="name"
             value={name}            
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]{4,8}*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+([' -]?[a-zA-Zа-яА-Я ]?[a-zA-Zа-яА-Я]{4,8})*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
@@ -58,7 +58,7 @@ export const Phonebook = ({ onAddContact }) => {
             value={number}
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
+            pattern="^\+?\d{1,4}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             placeholder="Введіть номер"
